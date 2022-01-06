3e225374c5512200c3323a67def16fe170d36762
+ <+>
2022-01-06 02:58:46 -0800

    commit tree 00ab02de33fad75454b2d5e1a4dbc4c50aaa4d62

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 62
+let num2 = 1
+const multiply = num1 * num2
 
-// Write your code here
+
+let num3 = 8
+let num4 = 2
+const mod = num3 / num4
+
+const set = (5, 15, 20)
+let max = 20
+
+
+let random = Math.ceil(Math.random() * 100)
